7da3895824e1d3e92ac7434736054beb9d8624a8
GideonRaymond <gideon.legrand@gmail.com>
2022-06-03 12:04:42 +0100

    Remove env from cache

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey: '3574c162-7972-4ad6-86c8-8240893b6c3a',
-  endPoints: {
-    breedList: 'https://api.thecatapi.com/v1/breeds',
-    categoriesList: 'https://api.thecatapi.com/v1/categories',
-    search: 'https://api.thecatapi.com/v1/images/search',
-  },
-};

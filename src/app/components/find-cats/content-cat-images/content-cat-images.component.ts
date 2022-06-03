2ffb0518717fce88ec8fde714ff82790d335e796
GideonRaymond <gideon.legrand@gmail.com>
2022-06-03 13:41:45 +0100

    Refactor button cleanup
    Move button to parent for cleaner code

--- src/app/components/find-cats/content-cat-images/content-cat-images.component.ts
+++ src/app/components/find-cats/content-cat-images/content-cat-images.component.ts
@@ -7,8 +7,4 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class ContentCatImagesComponent {
   /** List of retrieved urls corresponding to the cat-images. */
   @Input() catUrls: string[];
-  /** Drawer is either 'side' (always visible) or 'over' (hidden on small screens) */
-  @Input() drawerMode: string;
-  /** Emits when button clicked to open Drawer (small screens only). */
-  @Output() onDrawerToggle = new EventEmitter<void>();
 }
